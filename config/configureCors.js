3f8bfb47f217b3f0533c9667c2bf64ae94796924
shuantt <tehsuan.tht@gmail.com>
2024-06-17 23:28:06 +0800

    fix: 調整 cors 全開

--- config/configureCors.js
+++ config/configureCors.js
@@ -1,27 +1,30 @@
 // 根據環境設定 CORS 選項
 function setCorsOptions(env) {
   let corsOptions;
-  if (env === 'production') {
-    const whitelist = [
-      'https://www.example.com', 
-      'https://example.com',
-      'http://127.0.0.1:3000',
-      'http://localhost:3000'
-    ];
-    corsOptions = {
-      origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-          callback(null, true);
-        } else {
-          callback(new Error('禁止CORS請求'));
-        }
-      },
-    };
-  } else {
-    corsOptions = {
-      origin: '*',
-    };
-  }
+  corsOptions = {
+    origin: '*',
+  };
+  // if (env === 'production') {
+  //   const whitelist = [
+  //     'https://www.example.com', 
+  //     'https://example.com',
+  //     'http://127.0.0.1:3000',
+  //     'http://localhost:3000'
+  //   ];
+  //   corsOptions = {
+  //     origin: function (origin, callback) {
+  //       if (whitelist.indexOf(origin) !== -1 || !origin) {
+  //         callback(null, true);
+  //       } else {
+  //         callback(new Error('禁止CORS請求'));
+  //       }
+  //     },
+  //   };
+  // } else {
+  //   corsOptions = {
+  //     origin: '*',
+  //   };
+  // }
 
   return corsOptions;
 }
